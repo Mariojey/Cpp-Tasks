b5c1767338a6b38b12a97ed09b5f0e186cc0a11f
Mariusz Jacek <marios.yaceg@gmail.com>
2025-09-09 11:53:11 +0200

    Loading Items

--- Basic/GameOfTrade/main.cpp
+++ Basic/GameOfTrade/main.cpp
@@ -28,12 +28,40 @@ vector<Item> loadItemsFromFile(const string& filename){
     return items;
 }
 
+vector<Item> loadPlayerEquipment(const string& filename, int * playerMoney){
+
+    vector<Item> items;
+    ifstream file(filename);
+
+    if(!file.is_open()){
+        cerr<<"Zgubiłeś plecak"<<endl;
+        return items;
+    }
+
+    string name;
+    int typeInt;
+    int price;
+    int money;
+
+    while(file >> name >> typeInt >> price >> money){
+        items.emplace_back(name, static_cast<EItemType>(typeInt), price);
+        * playerMoney = money;
+    }
+
+    file.close();
+    return items;
+}
+
 int main(){
+
+    int playerMoney = 0;
+    vector<Item> playerEquipment = loadPlayerEquipment("player.txt", &playerMoney);
     
     vector<Item> loadedInkeeperItems = loadItemsFromFile("inkeeper.txt");
 
     Inkeeper John = Inkeeper("John", 20, loadedInkeeperItems);
     John.ShowItems();
+    cout<<playerMoney<<endl;
 
     return 0;
 }
