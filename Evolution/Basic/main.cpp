cb2ba7bba061536e12d119fae7145ef3d628a554
Mariusz Jacek <marios.yaceg@gmail.com>
2025-09-11 10:21:49 +0200

    Implementation  validation

--- Evolution/Basic/main.cpp
+++ Evolution/Basic/main.cpp
@@ -2,10 +2,35 @@
 #include <vector>
 #include <ctime>
 #include <cstdlib>
+#include <fstream>
 
 #include "Gen.h"
 
 using namespace std;
+
+unsigned int MAX_WEIGHT = 12;
+
+vector<GenCandidat> loadGensFromFile(const string& filename){
+
+    vector<GenCandidat> gens;
+    ifstream file(filename);
+
+    if(!file.is_open()){
+        cerr<<"Required file is unavailable!"<<endl;
+        return gens;
+    }
+
+    unsigned int id;
+    unsigned int price;
+    unsigned int weight;
+
+    while(file >> id >> price >> weight){
+        gens.emplace_back(id, price, weight);
+    }
+    
+    file.close();
+    return gens;
+}
 
 vector<vector<Gen>> generatePopulation(int populationSize, int chromosomSize){
     
@@ -33,11 +58,35 @@ vector<vector<Gen>> generatePopulation(int populationSize, int chromosomSize){
     
 }
 
+unsigned int chromosomValidation(vector<Gen> chromosom, int chromosomSize, vector<GenCandidat> potentialGens, unsigned int maxWeight){
+
+    unsigned int sumWeight = 0;
+    unsigned int sumPrice = 0;
+
+    for (int i = 0; i < chromosomSize; i++)
+    {
+        Gen current = chromosom[i];
+
+        if(current.getAllel() == true){
+            sumWeight += potentialGens[i].getWeight();
+            sumPrice += potentialGens[i].getPrice();
+        }
+
+        if(sumWeight > maxWeight){
+            return 0;
+        }
+    }
+
+    return sumPrice;
+
+}
+
 int main(){
 
     srand((unsigned)time(0));
 
     vector<vector<Gen>> generatedPopulation = generatePopulation(10, 8);
+    vector<GenCandidat> gensEnvironment = loadGensFromFile("stats.txt");
 
     for (auto& chromosom : generatedPopulation)
     {
@@ -46,6 +95,13 @@ int main(){
         }
         cout<<endl;
     }
+
+    for (auto& chromosom : generatedPopulation)
+    {
+        unsigned int calcualtedPrice = chromosomValidation(chromosom, 8, gensEnvironment, MAX_WEIGHT);
+        cout<<calcualtedPrice<<endl;
+    }
+    
     
 
     return 0;
